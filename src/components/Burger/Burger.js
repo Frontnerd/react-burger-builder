72ba86cae13254581d8624360783c34809003f39
Frontnerd <hi@frontnerd.com>
2019-01-28 15:30:16 +0100

    dynamic ingredient sum

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+  // tranform the main state Object (in BuregerBuilder)
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // into an Array of properties and values
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // that are a new Array if multiple (ex. cheese, cheese)
+        return (
+          <BurgerIngredient
+            key={igKey + i}
+            type={igKey} />
+          );
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
